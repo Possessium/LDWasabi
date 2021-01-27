750d9ac15e93faefed34d4e533df4d06b722d3dd
G <hugo.guerquin@outlook.fr>
2021-01-27 17:12:39 +0100

    Auto stash before merge of "Hugo" and "origin/Hugo"

---
+++ Assets/Tools/SceneLoader/Scripts/SceneLoaderProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoaderProfile : ScriptableObject
+{
+    // Names of the folders
+    public List<string> Folders = new List<string>();
+
+    // Boolean for each folder to activate or not the foldout
+    public List<bool> ActivatedFolders = new List<bool>();
+
+    // List containing another List of each scene in every folders
+    public List<ScenesData> ListedScenes = new List<ScenesData>();
+}
+
+[System.Serializable]
+public class ScenesData
+{
+    public List<string> Content = new List<string>();
+}
